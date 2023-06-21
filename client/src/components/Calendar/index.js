24647b4760ecd0632e061bf1d8b04ea32c9fb243
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-06-20 20:31:33 -0500

    Calendly Widget

---
+++ client/src/components/Calendar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {InlineWidget} from 'react-calendly'
+
+
+const Calendar = () => {
+  return (
+    <div>
+    <InlineWidget url="https://calendly.com/jpbaertsch/school-tour" />
+  </div>
+  )
+}
+
+export default Calendar
