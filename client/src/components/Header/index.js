eccf3202ad7bcaa5564556e5dcae9d42fb5b5065
tykervella <kervellatyler@gmail.com>
2023-06-21 17:31:02 -0700

    more homepage flexbox styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,87 +5,90 @@ import logo from "../../assets/logo.png";
 
 const CustomNavbar = () => {
   return (
-    <Navbar
-      variant="dark"
-      expand="lg"
-      className="text-white mb-4 py-3 mt-100"
-      id="custom-navbar"
-      style={{ backgroundColor: "#5271ff", color: "#ffffff" }}
-    >
-      <Navbar.Brand id="logo">
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Logo"
-            style={{ width: "175px" }}
-          />
-        </Link>
-      </Navbar.Brand>
+    <div>
+      {/* <div style={{ backgroundColor: "#ffffff", height: "30px" }}></div> */}
+      <Navbar
+        variant="dark"
+        expand="lg"
+        className="text-white mb-4 py-3 mt-100"
+        id="custom-navbar"
+        style={{ backgroundColor: "#5271ff", color: "#ffffff" }}
+      >
+        <Navbar.Brand id="logo">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "175px" }}
+            />
+          </Link>
+        </Navbar.Brand>
 
-      <Container id="dropdown-titles">
-        <NavDropdown title="About Us" id="nav-dropdown-about-us" className="nav-dropdown" >
-          <div className="dropdown-items-container">
-            <NavDropdown.Item as={Link} to="#">
-              General Information
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Rates & Fees
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Careers
-            </NavDropdown.Item>
-          </div>
-        </NavDropdown>
+        <Container id="dropdown-titles">
+          <NavDropdown title="About Us" id="nav-dropdown-about-us" className="nav-dropdown" >
+            <div className="dropdown-items-container">
+              <NavDropdown.Item as={Link} to="#">
+                General Information
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Rates & Fees
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Careers
+              </NavDropdown.Item>
+            </div>
+          </NavDropdown>
 
-        <NavDropdown title="Enrollment" id="nav-dropdown-enrollment" className="nav-dropdown">
-          <div className="dropdown-items-container">
-            <NavDropdown.Item as={Link} to="/calendar">
-              Schedule a Tour
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Waitlist
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Forms
-            </NavDropdown.Item>
-          </div>
-        </NavDropdown>
+          <NavDropdown title="Enrollment" id="nav-dropdown-enrollment" className="nav-dropdown">
+            <div className="dropdown-items-container">
+              <NavDropdown.Item as={Link} to="/calendar">
+                Schedule a Tour
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Waitlist
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Forms
+              </NavDropdown.Item>
+            </div>
+          </NavDropdown>
 
-        <NavDropdown title="Classrooms" id="nav-dropdown-classrooms" className="nav-dropdown">
-          <div className="dropdown-items-container">
-            <NavDropdown.Item as={Link} to="#">
-              Room 237
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Room 241
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Room 243
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Room 341
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Room 345
-            </NavDropdown.Item>
-          </div>
-        </NavDropdown>
+          <NavDropdown title="Classrooms" id="nav-dropdown-classrooms" className="nav-dropdown">
+            <div className="dropdown-items-container">
+              <NavDropdown.Item as={Link} to="#">
+                Room 237
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Room 241
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Room 243
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Room 341
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Room 345
+              </NavDropdown.Item>
+            </div>
+          </NavDropdown>
 
-        <NavDropdown title="Resources" id="nav-dropdown-resources" className="nav-dropdown">
-          <div className="dropdown-items-container">
-            <NavDropdown.Item as={Link} to="#">
-              Menus
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              Days Closed
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="#">
-              FAQs
-            </NavDropdown.Item>
-          </div>
-        </NavDropdown>
-      </Container>
-    </Navbar>
+          <NavDropdown title="Resources" id="nav-dropdown-resources" className="nav-dropdown">
+            <div className="dropdown-items-container">
+              <NavDropdown.Item as={Link} to="#">
+                Menus
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                Days Closed
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="#">
+                FAQs
+              </NavDropdown.Item>
+            </div>
+          </NavDropdown>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
